99c4f950d66fa0c1d8706aee41a1850f550af201
ang9009 <ngalvin2004@gmail.com>
2025-08-06 08:04:56 +0400

    Finish transmit

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -6,26 +6,25 @@
 #include "proc.h"
 #include "defs.h"
 #include "e1000_dev.h"
+#include "net.h"
 
 #define TX_RING_SIZE 16
 static struct tx_desc tx_ring[TX_RING_SIZE] __attribute__((aligned(16)));
-static char *tx_bufs[TX_RING_SIZE];
+static char* tx_bufs[TX_RING_SIZE];
 
 #define RX_RING_SIZE 16
 static struct rx_desc rx_ring[RX_RING_SIZE] __attribute__((aligned(16)));
-static char *rx_bufs[RX_RING_SIZE];
+static char* rx_bufs[RX_RING_SIZE];
 
 // remember where the e1000's registers live.
-static volatile uint32 *regs;
+static volatile uint32* regs;
 
 struct spinlock e1000_lock;
 
 // called by pci_init().
 // xregs is the memory address at which the
 // e1000's registers are mapped.
-void
-e1000_init(uint32 *xregs)
-{
+void e1000_init(uint32* xregs) {
   int i;
 
   initlock(&e1000_lock, "e1000");
@@ -33,9 +32,9 @@ e1000_init(uint32 *xregs)
   regs = xregs;
 
   // Reset the device
-  regs[E1000_IMS] = 0; // disable interrupts
+  regs[E1000_IMS] = 0;  // disable interrupts
   regs[E1000_CTL] |= E1000_CTL_RST;
-  regs[E1000_IMS] = 0; // redisable interrupts
+  regs[E1000_IMS] = 0;  // redisable interrupts
   __sync_synchronize();
 
   // [E1000 14.5] Transmit initialization
@@ -44,22 +43,22 @@ e1000_init(uint32 *xregs)
     tx_ring[i].status = E1000_TXD_STAT_DD;
     tx_bufs[i] = 0;
   }
-  regs[E1000_TDBAL] = (uint64) tx_ring;
-  if(sizeof(tx_ring) % 128 != 0)
+  regs[E1000_TDBAL] = (uint64)tx_ring;
+  if (sizeof(tx_ring) % 128 != 0)
     panic("e1000");
   regs[E1000_TDLEN] = sizeof(tx_ring);
   regs[E1000_TDH] = regs[E1000_TDT] = 0;
-  
+
   // [E1000 14.4] Receive initialization
   memset(rx_ring, 0, sizeof(rx_ring));
   for (i = 0; i < RX_RING_SIZE; i++) {
     rx_bufs[i] = kalloc();
     if (!rx_bufs[i])
       panic("e1000");
-    rx_ring[i].addr = (uint64) rx_bufs[i];
+    rx_ring[i].addr = (uint64)rx_bufs[i];
   }
-  regs[E1000_RDBAL] = (uint64) rx_ring;
-  if(sizeof(rx_ring) % 128 != 0)
+  regs[E1000_RDBAL] = (uint64)rx_ring;
+  if (sizeof(rx_ring) % 128 != 0)
     panic("e1000");
   regs[E1000_RDH] = 0;
   regs[E1000_RDT] = RX_RING_SIZE - 1;
@@ -67,60 +66,67 @@ e1000_init(uint32 *xregs)
 
   // filter by qemu's MAC address, 52:54:00:12:34:56
   regs[E1000_RA] = 0x12005452;
-  regs[E1000_RA+1] = 0x5634 | (1<<31);
+  regs[E1000_RA + 1] = 0x5634 | (1 << 31);
   // multicast table
-  for (int i = 0; i < 4096/32; i++)
+  for (int i = 0; i < 4096 / 32; i++)
     regs[E1000_MTA + i] = 0;
 
   // transmitter control bits.
-  regs[E1000_TCTL] = E1000_TCTL_EN |  // enable
-    E1000_TCTL_PSP |                  // pad short packets
-    (0x10 << E1000_TCTL_CT_SHIFT) |   // collision stuff
-    (0x40 << E1000_TCTL_COLD_SHIFT);
-  regs[E1000_TIPG] = 10 | (8<<10) | (6<<20); // inter-pkt gap
+  regs[E1000_TCTL] = E1000_TCTL_EN |                  // enable
+                     E1000_TCTL_PSP |                 // pad short packets
+                     (0x10 << E1000_TCTL_CT_SHIFT) |  // collision stuff
+                     (0x40 << E1000_TCTL_COLD_SHIFT);
+  regs[E1000_TIPG] = 10 | (8 << 10) | (6 << 20);  // inter-pkt gap
 
   // receiver control bits.
-  regs[E1000_RCTL] = E1000_RCTL_EN | // enable receiver
-    E1000_RCTL_BAM |                 // enable broadcast
-    E1000_RCTL_SZ_2048 |             // 2048-byte rx buffers
-    E1000_RCTL_SECRC;                // strip CRC
-  
+  regs[E1000_RCTL] = E1000_RCTL_EN |       // enable receiver
+                     E1000_RCTL_BAM |      // enable broadcast
+                     E1000_RCTL_SZ_2048 |  // 2048-byte rx buffers
+                     E1000_RCTL_SECRC;     // strip CRC
+
   // ask e1000 for receive interrupts.
-  regs[E1000_RDTR] = 0; // interrupt after every received packet (no timer)
-  regs[E1000_RADV] = 0; // interrupt after every packet (no timer)
-  regs[E1000_IMS] = (1 << 7); // RXDW -- Receiver Descriptor Write Back
+  regs[E1000_RDTR] = 0;  // interrupt after every received packet (no timer)
+  regs[E1000_RADV] = 0;  // interrupt after every packet (no timer)
+  regs[E1000_IMS] = (1 << 7);  // RXDW -- Receiver Descriptor Write Back
 }
 
-int
-e1000_transmit(char *buf, int len)
-{
-  //
-  // Your code here.
-  //
-  // buf contains an ethernet frame; program it into
-  // the TX descriptor ring so that the e1000 sends it. Stash
-  // a pointer so that it can be freed after send completes.
-  //
+int e1000_transmit(char* buf, int len) {
+  acquire(&e1000_lock);
+  // Free buffer in next descriptor if possible
+  uint32 next_idx = regs[E1000_TDT];
+  struct tx_desc* desc = &tx_ring[next_idx];
+  if (!(desc->status & E1000_TXD_STAT_DD)) {
+    release(&e1000_lock);
+    return -1;
+  }
+  if (desc->addr) {
+    kfree((void*)desc->addr);
+  }
 
-  
+  // Set command flags and buffer address
+  desc->cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  desc->addr = (uint64)buf;
+  desc->length = (uint16)len;
+
+  // Update ring position
+  regs[E1000_TDT] = (regs[E1000_TDT] + 1) % TX_RING_SIZE;
+  release(&e1000_lock);
+  printf("success\n");
+
   return 0;
 }
 
-static void
-e1000_recv(void)
-{
+static void e1000_recv(void) {
+  printf("receive\n");
   //
   // Your code here.
   //
   // Check for packets that have arrived from the e1000
   // Create and deliver a buf for each packet (using net_rx()).
   //
-
 }
 
-void
-e1000_intr(void)
-{
+void e1000_intr(void) {
   // tell the e1000 we've seen this interrupt;
   // without this the e1000 won't raise any
   // further interrupts.
--- kernel/net.c
+++ kernel/net.c
@@ -11,29 +11,24 @@
 #include "net.h"
 
 // xv6's ethernet and IP addresses
-static uint8 local_mac[ETHADDR_LEN] = { 0x52, 0x54, 0x00, 0x12, 0x34, 0x56 };
+static uint8 local_mac[ETHADDR_LEN] = {0x52, 0x54, 0x00, 0x12, 0x34, 0x56};
 static uint32 local_ip = MAKE_IP_ADDR(10, 0, 2, 15);
 
 // qemu host's ethernet address.
-static uint8 host_mac[ETHADDR_LEN] = { 0x52, 0x55, 0x0a, 0x00, 0x02, 0x02 };
+static uint8 host_mac[ETHADDR_LEN] = {0x52, 0x55, 0x0a, 0x00, 0x02, 0x02};
 
 static struct spinlock netlock;
 
-void
-netinit(void)
-{
+void netinit(void) {
   initlock(&netlock, "netlock");
 }
-
 
 //
 // bind(int port)
 // prepare to receive UDP packets address to the port,
 // i.e. allocate any queues &c needed.
 //
-uint64
-sys_bind(void)
-{
+uint64 sys_bind(void) {
   //
   // Your code here.
   //
@@ -46,9 +41,7 @@ sys_bind(void)
 // release any resources previously created by bind(port);
 // from now on UDP packets addressed to port should be dropped.
 //
-uint64
-sys_unbind(void)
-{
+uint64 sys_unbind(void) {
   //
   // Optional: Your code here.
   //
@@ -71,9 +64,7 @@ sys_unbind(void)
 // dport, *src, and *sport are host byte order.
 // bind(dport) must previously have been called.
 //
-uint64
-sys_recv(void)
-{
+uint64 sys_recv(void) {
   //
   // Your code here.
   //
@@ -82,11 +73,9 @@ sys_recv(void)
 
 // This code is lifted from FreeBSD's ping.c, and is copyright by the Regents
 // of the University of California.
-static unsigned short
-in_cksum(const unsigned char *addr, int len)
-{
+static unsigned short in_cksum(const unsigned char* addr, int len) {
   int nleft = len;
-  const unsigned short *w = (const unsigned short *)addr;
+  const unsigned short* w = (const unsigned short*)addr;
   unsigned int sum = 0;
   unsigned short answer = 0;
 
@@ -95,14 +84,14 @@ in_cksum(const unsigned char *addr, int len)
    * sequential 16 bit words to it, and at the end, fold back all the
    * carry bits from the top 16 bits into the lower 16 bits.
    */
-  while (nleft > 1)  {
+  while (nleft > 1) {
     sum += *w++;
     nleft -= 2;
   }
 
   /* mop up an odd byte, if necessary */
   if (nleft == 1) {
-    *(unsigned char *)(&answer) = *(const unsigned char *)w;
+    *(unsigned char*)(&answer) = *(const unsigned char*)w;
     sum += answer;
   }
 
@@ -118,10 +107,8 @@ in_cksum(const unsigned char *addr, int len)
 //
 // send(int sport, int dst, int dport, char *buf, int len)
 //
-uint64
-sys_send(void)
-{
-  struct proc *p = myproc();
+uint64 sys_send(void) {
+  struct proc* p = myproc();
   int sport;
   int dst;
   int dport;
@@ -135,23 +122,23 @@ sys_send(void)
   argint(4, &len);
 
   int total = len + sizeof(struct eth) + sizeof(struct ip) + sizeof(struct udp);
-  if(total > PGSIZE)
+  if (total > PGSIZE)
     return -1;
 
-  char *buf = kalloc();
-  if(buf == 0){
+  char* buf = kalloc();
+  if (buf == 0) {
     printf("sys_send: kalloc failed\n");
     return -1;
   }
   memset(buf, 0, PGSIZE);
 
-  struct eth *eth = (struct eth *) buf;
+  struct eth* eth = (struct eth*)buf;
   memmove(eth->dhost, host_mac, ETHADDR_LEN);
   memmove(eth->shost, local_mac, ETHADDR_LEN);
   eth->type = htons(ETHTYPE_IP);
 
-  struct ip *ip = (struct ip *)(eth + 1);
-  ip->ip_vhl = 0x45; // version 4, header length 4*5
+  struct ip* ip = (struct ip*)(eth + 1);
+  ip->ip_vhl = 0x45;  // version 4, header length 4*5
   ip->ip_tos = 0;
   ip->ip_len = htons(sizeof(struct ip) + sizeof(struct udp) + len);
   ip->ip_id = 0;
@@ -160,15 +147,15 @@ sys_send(void)
   ip->ip_p = IPPROTO_UDP;
   ip->ip_src = htonl(local_ip);
   ip->ip_dst = htonl(dst);
-  ip->ip_sum = in_cksum((unsigned char *)ip, sizeof(*ip));
+  ip->ip_sum = in_cksum((unsigned char*)ip, sizeof(*ip));
 
-  struct udp *udp = (struct udp *)(ip + 1);
+  struct udp* udp = (struct udp*)(ip + 1);
   udp->sport = htons(sport);
   udp->dport = htons(dport);
   udp->ulen = htons(len + sizeof(struct udp));
 
-  char *payload = (char *)(udp + 1);
-  if(copyin(p->pagetable, payload, bufaddr, len) < 0){
+  char* payload = (char*)(udp + 1);
+  if (copyin(p->pagetable, payload, bufaddr, len) < 0) {
     kfree(buf);
     printf("send: copyin failed\n");
     return -1;
@@ -179,19 +166,16 @@ sys_send(void)
   return 0;
 }
 
-void
-ip_rx(char *buf, int len)
-{
+void ip_rx(char* buf, int len) {
   // don't delete this printf; make grade depends on it.
   static int seen_ip = 0;
-  if(seen_ip == 0)
+  if (seen_ip == 0)
     printf("ip_rx: received an IP packet\n");
   seen_ip = 1;
 
   //
   // Your code here.
   //
-  
 }
 
 //
@@ -201,31 +185,31 @@ ip_rx(char *buf, int len)
 // qemu to send IP packets to xv6; the real ARP
 // protocol is more complex.
 //
-void
-arp_rx(char *inbuf)
-{
+void arp_rx(char* inbuf) {
   static int seen_arp = 0;
 
-  if(seen_arp){
+  if (seen_arp) {
     kfree(inbuf);
     return;
   }
   printf("arp_rx: received an ARP packet\n");
   seen_arp = 1;
 
-  struct eth *ineth = (struct eth *) inbuf;
-  struct arp *inarp = (struct arp *) (ineth + 1);
+  struct eth* ineth = (struct eth*)inbuf;
+  struct arp* inarp = (struct arp*)(ineth + 1);
 
-  char *buf = kalloc();
-  if(buf == 0)
+  char* buf = kalloc();
+  if (buf == 0)
     panic("send_arp_reply");
-  
-  struct eth *eth = (struct eth *) buf;
-  memmove(eth->dhost, ineth->shost, ETHADDR_LEN); // ethernet destination = query source
-  memmove(eth->shost, local_mac, ETHADDR_LEN); // ethernet source = xv6's ethernet address
+
+  struct eth* eth = (struct eth*)buf;
+  memmove(eth->dhost, ineth->shost,
+          ETHADDR_LEN);  // ethernet destination = query source
+  memmove(eth->shost, local_mac,
+          ETHADDR_LEN);  // ethernet source = xv6's ethernet address
   eth->type = htons(ETHTYPE_ARP);
 
-  struct arp *arp = (struct arp *)(eth + 1);
+  struct arp* arp = (struct arp*)(eth + 1);
   arp->hrd = htons(ARP_HRD_ETHER);
   arp->pro = htons(ETHTYPE_IP);
   arp->hln = ETHADDR_LEN;
@@ -242,16 +226,14 @@ arp_rx(char *inbuf)
   kfree(inbuf);
 }
 
-void
-net_rx(char *buf, int len)
-{
-  struct eth *eth = (struct eth *) buf;
+void net_rx(char* buf, int len) {
+  struct eth* eth = (struct eth*)buf;
 
-  if(len >= sizeof(struct eth) + sizeof(struct arp) &&
-     ntohs(eth->type) == ETHTYPE_ARP){
+  if (len >= sizeof(struct eth) + sizeof(struct arp) &&
+      ntohs(eth->type) == ETHTYPE_ARP) {
     arp_rx(buf);
-  } else if(len >= sizeof(struct eth) + sizeof(struct ip) &&
-     ntohs(eth->type) == ETHTYPE_IP){
+  } else if (len >= sizeof(struct eth) + sizeof(struct ip) &&
+             ntohs(eth->type) == ETHTYPE_IP) {
     ip_rx(buf, len);
   } else {
     kfree(buf);
